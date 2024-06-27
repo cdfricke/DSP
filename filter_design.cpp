62ae91e59c641b620cf9474130eff794d7015678
Connor Fricke <cd.fricke23@gmail.com>
2024-06-27 12:57:21 -0400

    3-component example signal

--- filter_design.cpp
+++ filter_design.cpp
@@ -20,35 +20,38 @@ int main()
     // parameters
     bool FIR = false;                       // decides which filter is applied. Both have two taps chosen by alpha
     double alpha = 0.125;                   // filter coeffs are {a, 1-a}
-    double SIGNAL_DURATION = 100.0;          // generated signal duration in seconds. we want this to be long to improve DFT result
-    double SAMPLING_RATE = 2.0;
+    double SAMPLING_RATE = 12.0e+06;        // 12 MHz CMOD clock frequency
     double SAMPLING_PERIOD = 1.0 / SAMPLING_RATE;
     double NYQUIST = SAMPLING_RATE / 2.0;
-
-    // *** SAMPLING CONTROL ***
-    vector<double> t_Samples;
-    for (double i = 0.0; i < SIGNAL_DURATION; i += SAMPLING_PERIOD) t_Samples.push_back(i);
-    int N = t_Samples.size();
-    // *** FREQUENCY (k) DISCRETIZATION ***
-    vector<int> k_vals;
-    for (int i = -N / 2; i < N / 2; i++) k_vals.push_back(i);
 
     // *** USER-CREATED SIGNAL ***
     // to create a signal with sine wave components, you need a vector of SignalComponents,
     // and a vector of discrete t values which tells us where the samples of the signal are.
     // The SignalComponent initialized as {0.1, 5.0} corresponds to 0.2sin(5.0t) in the Fourier series, for example.
     // if we create a signal with all frequencies from zero to nyquist, we should get an impulsive signal
-    vector<SignalComponent> components;
-    for (double freq = 0.00; freq < NYQUIST; freq += 0.01)
-    {
-        SignalComponent newComp = {1.0, freq};
-        components.push_back(newComp);
-    }
+    vector<SignalComponent> components{{1.0, 500e+03}, {0.5, 1e+06}, {0.25, 2e+06}};
+    // find minimum and maximum components
+    double minFreq = components[0].freq;
+    double maxFreq = components[0].freq;
     for (SignalComponent c : components)
     {
-        assert(c.freq < NYQUIST);
+        if (c.freq < minFreq) minFreq = c.freq;
+        if (c.freq > maxFreq) maxFreq = c.freq;
     }
+    assert(maxFreq < NYQUIST);
+
+    double SIGNAL_DURATION = 2.0 / minFreq; // should be on the order of the period of the lowest freq component in our signal
+
+    // *** SAMPLING CONTROL ***
+    vector<double> t_Samples;
+    for (double i = 0.0; i < SIGNAL_DURATION; i += SAMPLING_PERIOD) t_Samples.push_back(i);
+    int N = t_Samples.size();
+    // once we have the signal components and sampling control set, we can call generateSignal()
     vector<double> x = generateSignal(t_Samples, components);
+
+    // *** FREQUENCY (k) DISCRETIZATION ***
+    vector<int> k_vals;
+    for (int i = -N / 2; i < N / 2; i++) k_vals.push_back(i);
 
     // *** DFT ***
     auto startDFT = high_resolution_clock::now();
